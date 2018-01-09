b6af35f720a2c8a5a5da16aeb6d020298347736a
Gloria <tie.h.gloria@gmail.com>
2018-01-09 17:00:19 -0500

    Swipe Button functionality changed using Butterknife

--- app/src/main/java/com/casept/yummm/MainActivity.java
+++ app/src/main/java/com/casept/yummm/MainActivity.java
@@ -6,29 +6,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    //@BindView(R.id.btnBuySwipe)
-    //Button btnBuySwipe;
+    @BindView(R.id.btnBuySwipe)
+    Button btnBuySwipe;
 
-    private Button btnBuySwipe;
+    //private Button btnBuySwipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
-        btnBuySwipe = (Button) findViewById(R.id.btnBuySwipe);
+    }
 
-        btnBuySwipe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                launchActivity();
-            }
-        });
+    @OnClick(R.id.btnBuySwipe)
+    public void onClick(View view){
+        launchActivity();
     }
 
     private void launchActivity() {
@@ -36,12 +36,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, VenueActivity.class);
         startActivity(intent);
     }
-
-    /*@OnClick(R.id.btnBuySwipe)
-    public void displayVenues(View view){
-        Intent intent = new Intent(view.getContext(), VenueActivity.class);
-        startActivityForResult(intent, 0);
-    }*/
-
 
 }
--- app/src/main/java/com/casept/yummm/VenueActivity.java
+++ app/src/main/java/com/casept/yummm/VenueActivity.java
@@ -1,26 +1,40 @@
 package com.casept.yummm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class VenueActivity extends AppCompatActivity {
 
-    private Button btnBack;
+    //private Button btnBack;
+
+    @BindView(R.id.btnBack)
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue);
 
-        btnBack = (Button) findViewById(R.id.btnBack);
+        ButterKnife.bind(this);
 
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
     }
+
+    @OnClick(R.id.btnBack)
+    public void onClick(View view){
+        launchActivity();
+    }
+
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
